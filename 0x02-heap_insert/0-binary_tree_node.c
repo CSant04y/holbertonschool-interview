f06b2f22dd0cf2ca07b2a6bde69a24e659a326a8
CSant04y <esquivelcarlos27893@gmail.com>
2021-08-05 16:55:34 +0000

    added task

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,28 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node()
- * 
- * 
- * 
- */ 
+ * binary_tree_node - adds node to binary tree
+ * @parent: parent pointer
+ * @value: Value to be stored in node
+ * Return: pointer to node
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    /*create a pointer for the new node*/
-    binary_tree_t *node;
+	/*create a pointer for the new node*/
+	binary_tree_t *node;
 
-    node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    /* Checking for failure */
-    if (!node)
-        return (NULL);
+	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	/* Checking for failure */
+	if (!node)
+		return (NULL);
 
-    /*Initilize node pointer to parent node*/
+	/*Initilize node pointer to parent node*/
 
-    node->parent = parent;
-    /*Initilize node with given value*/
-    node->n = value;
-    node->left = NULL;
-    node->right = NULL;
+	node->parent = parent;
+	/*Initilize node with given value*/
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 
-    return (node);
+	return (node);
 }
