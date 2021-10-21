ab6aea2558a00748dec29cdc1eea906ee2af4585
CSant04y <esquivelcarlos27893@gmail.com>
2021-10-21 19:31:29 +0000

    dded task

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+"""[This is the N-queens problem in which N ammount of queens
+    are places on a chess board of n size]
+"""
+
+from sys import argv
+
+
+def checkPosition(board, i, j, col):
+    """[summary]
+
+    Args:
+        board ([list]): [description]
+        i ([int]): [description]
+        j ([int]): [description]
+        col ([int]): [de]
+    """
+
+    if ((board[i] == j) or (board[i] == i - j + col) or
+            (board[i] == j - col + i)):
+        return True
+    return False
+
+
+def printTable(table, N):
+    """[This prints the table]
+
+    Args:
+        table ([type]): [description]
+        N ([type]): [description]
+    """
+    board = []
+
+    for row in range(N):
+        for col in range(N):
+            if col == table[row]:
+                board.append([row, col])
+    print(board)
+
+
+def findSolution(board, col, N):
+    """[summary]
+
+    Args:
+        board ([List]): [List of list of ]
+        col ([type]): [description]
+    """
+    if col == N:
+        printTable(board, N)
+
+    else:
+        for j in range(N):
+            mark = True
+            for i in range(col):
+                if checkPosition(board, i, j, col):
+                    mark = False
+            if mark:
+                board[col] = j
+                findSolution(board, col + 1, N)
+
+
+def solveNQ():
+    """[This function solves the N- Queen problem]
+
+    Args:
+        argv ([int]): [Int that indicated the size of N]
+    """
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    try:
+        N = int(argv[1])
+    except BaseException:
+        print("N must be a number")
+        exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+
+    board = [[x for x in range(int(argv[1]))] for i in range(int(argv[1]))]
+
+    col = 0
+    findSolution(board, col, N)
+
+solveNQ()
