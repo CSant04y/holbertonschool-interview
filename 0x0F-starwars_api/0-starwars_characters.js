a8db12c702fe96f2c26bd5a706ee1a76af401767
CSant04y <esquivelcarlos27893@gmail.com>
2021-11-11 21:01:39 +0000

    added semistandard fixes

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,12 +3,10 @@ const request = require('request');
 
 let characters = [];
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv.slice(2)}/`;
-console.log(url);
 request(url, function (error, response, body) {
   if (error || response.statusCode !== 200) console.log(error);
 
   else characters = JSON.parse(body).characters;
-  console.log('hello');
 
   const size = Object.keys(characters).length;
   const array = Array(size).fill();
@@ -16,11 +14,11 @@ request(url, function (error, response, body) {
   let data = 0;
 
   for (let i = 0; i < size; i++) {
-    request(characters[i], (error, response, body) => {
-      if (error || response.statusCode !== 200) console.log(error);
+    request(characters[i], (err, resp, bod) => {
+      if (err || response.statusCode !== 200) console.log(err);
 
       else {
-        array[i] = JSON.parse(body).name;
+        array[i] = JSON.parse(bod).name;
         data++;
       }
 
