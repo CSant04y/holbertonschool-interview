37b1909f191bbea5dbcb98e293743ec52caead42
CSant04y <esquivelcarlos27893@gmail.com>
2022-02-16 11:49:30 -0600

    added algo

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This function finds the permiter of an Island
+on a 2-demensional matrix
+"""
+
+def island_perimeter(grid):
+    '''Takes in grid and returns the perimeter'''
+    # Define vars to hold lens
+    grid_len, grid_row_len = len(grid), len(grid[0])
+
+    # Now make vars for land so that we can calculate math as
+    # As well as the neighboring land a.k.a vecinos
+    land, vecino = 0, 0
+
+    # Loop through length of grid first to access individual rows
+    for i in range(grid_len):
+        # Loop though lenght of row
+        for j in range(grid_row_len):
+            # Check to see if current position is equal to 1
+            if grid[i][j] == 1:
+                # add one to land == 4 perimeter
+                land += 1
+
+                # check for neighbors
+
+                if i < grid_len - 1 and grid[i + 1][j] == 1:
+                    vecino += 1
+                if j < grid_row_len - 1 and grid[i][j + 1] == 1:
+                    vecino += 1
+    # Return calculation of land * 4
+    return land * 4 - 2 * vecino
---
+++ 0x1C-island_perimeter/README.md
@@ -0,0 +1,12 @@
+# 0x1C-island_perimeter
+
+Create a function def *island_perimeter(grid):*
+
+## grid is a list of list of integers:
+
+* 0 represents water
+* 1 represents land
+* Each cell is square, with a side length of 1
+* Cells are connected horizontally/vertically (not diagonally).
+* grid is rectangular, with its width and height not exceeding 100
+
